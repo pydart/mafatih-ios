7016d30e0cacaa828ae719938be22105db45479c
Msh <mesh1368@gmail.com>
2021-04-11 21:34:35 +0200

    migrated to flutter 2 successfully

--- android/app/src/main/kotlin/msh/mehrdad/mafatih/MainActivity.kt
+++ android/app/src/main/kotlin/msh/mehrdad/mafatih/MainActivity.kt
@@ -1,13 +1,13 @@
 package msh.mehrdad.mafatih
 
-import android.os.Bundle
+//import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    GeneratedPluginRegistrant.registerWith(this)
+//  }
 }
